366cd48618e3fdd2fd429340863c4cf5ef4402e9
Sjoerd de Vries <sjdv1982@gmail.com>
2024-05-29 01:27:00 +0200

    Dask deployment working

--- dask/build-environment.sh
+++ dask/build-environment.sh
@@ -1,7 +1,7 @@
 set -u -e
 echo 'Check if we are inside a Seamless conda environment'
 echo 'If not, you can build one using: '
-echo '  mamba env create -n seamless-dask --file https://raw.githubusercontent.com/sjdv1982/seamless/stable/conda/seamless-framework-environment.yml'
+echo '  mamba env create -n seamless-dask --file https://raw.githubusercontent.com/sjdv1982/seamless/stable/conda/seamless-exact-environment.yml'
 echo '  conda activate seamless-dask'
 python -c 'import seamless'
 mamba env update --file dask-environment-update.yaml
---
+++ dask/example-config.sh
@@ -0,0 +1,8 @@
+export RANDOM_PORT_START=60001
+export RANDOM_PORT_END=61000
+
+export TMPDIR=/scratch/user
+
+# may need to be redefined/copied for each project:
+export ENVIRONMENT_OUTPUT_FILE=$HOME/seamless-deployment/run-db-hashserver-setup.sh
+export SEAMLESS_DASK_CONDA_ENVIRONMENT=seamless-dask
