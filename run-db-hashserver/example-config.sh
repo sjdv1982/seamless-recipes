2bbfccbce00912167bf1f1e41a6daf412ca43024
Sjoerd De Vries <sdevries@mbi-frontend.loria.fr>
2024-05-28 14:56:25 +0200

    snapshot

---
+++ run-db-hashserver/example-config.sh
@@ -0,0 +1,10 @@
+export CONDA_RUN_DB_HASHSERVER_ENVIRONMENT=run-db-hashserver
+
+export RANDOM_PORT_START=60001
+export RANDOM_PORT_END=61000
+
+export HASHSERVER_BUFFER_DIR=/home/user/seamless/buffers
+export DATABASE_DIR=/home/user/seamless/database
+
+# may need to be redefined/copied for each project:
+export ENVIRONMENT_OUTPUT_FILE=/home/user/seamless-deployment-env.sh
